4f62fb036e22085018586c625e9490a08061849a
Stropheum <stropheum060@gmail.com>
2024-12-26 13:36:27 -0500

    added credited title grid

--- PortfolioSite/ClientApp/src/components/Home.jsx
+++ PortfolioSite/ClientApp/src/components/Home.jsx
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
+import LogoGrid from "./LogoGrid";
 
 export class Home extends Component {
-    static displayName = Home.name;
+    static displayName = this.name;
 
     render() {
         return (
             <div>
                 <h1>Dale Diaz</h1>
-                <h2>Senior Game Programmer | Unity Expert | Polymath | Lifetime Learner</h2>
+                <h3>Senior Game Programmer | Unity Expert | Polymath | Lifetime Learner</h3>
                 <hr/>
-                <h3>About Me</h3>
+                <h1 align="center">About Me</h1>
                 <p>
                     I’m a game programmer who thrives at the intersection of art, science, and problem-solving.
                     My academic journey began in sound recording technology, where I explored the rich interplay between
@@ -36,20 +37,10 @@ export class Home extends Component {
                     I bring curiosity, adaptability, and a passion for creating games that marry technical precision
                     with creative vision.
                 </p>
-
-                {/*<p>Welcome to your new single-page application, built with:</p>*/}
-                {/*<ul>*/}
-                {/*  <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>*/}
-                {/*  <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>*/}
-                {/*  <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>*/}
-                {/*</ul>*/}
-                {/*<p>To help you get started, we have also set up:</p>*/}
-                {/*<ul>*/}
-                {/*  <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>*/}
-                {/*  <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>*/}
-                {/*  <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>*/}
-                {/*</ul>*/}
-                {/*<p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>*/}
+                
+                <hr />
+                <h1 align="center">Credited Titles</h1>
+                <LogoGrid/>
             </div>
         );
     }
