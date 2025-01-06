0253ffb04d31e841b8cf25e06abca16bb983e60d
Stropheum <stropheum060@gmail.com>
2025-01-05 22:20:32 -0500

    added hex visualizer embed and github entry

--- PortfolioSite/ClientApp/src/components/Samples.jsx
+++ PortfolioSite/ClientApp/src/components/Samples.jsx
@@ -1,5 +1,25 @@
 ﻿import React, {Component} from 'react';
+// import React from "react";
+import YouTube from "react-youtube";
 
+const VideoEmbed = () => {
+    const opts = {
+        height: "576",
+        width: "1024",
+        quality: "1080",
+        playerVars: {
+            autoplay: 1, // Autoplay the video
+        },
+    };
+
+    return (
+        <div>
+            <h2>Hex Visualizer</h2>
+            <h3>Leveraging DOTS and signal processing to make pretty things to look at</h3>
+            <YouTube videoId="8QoQzeEf6jY" opts={opts} />
+        </div>
+    );
+};
 export class Samples extends Component {
     static displayName = Samples.name;
 
@@ -7,7 +27,10 @@ export class Samples extends Component {
         return (
             <div>
                 <h1>Samples</h1>
-                <h2>Under Construction - Check back soon for cool toys to play with!</h2>
+                <h2>Some videos and embedded demos of my projects</h2>
+                <hr/>
+                <VideoEmbed />
+                <hr/>
             </div>
         );
     }
