f440cf09b1d7ac0fee3ddb7bb28519e9e8682e98
Stropheum <stropheum060@gmail.com>
2025-02-11 21:32:04 -0500

    bump

--- PortfolioSite/ClientApp/src/components/Samples.jsx
+++ PortfolioSite/ClientApp/src/components/Samples.jsx
@@ -38,7 +38,7 @@ const VideoEmbed = () => {
             </div>
             <hr/>
 
-            {/*Logician*/}
+            {/*Logiciannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn*/}
             <div className="card-no-alignment">
                 <div style={{display: "flex", flexDirection: "column", marginLeft: "0"}}>
                     <div>
